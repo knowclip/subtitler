663b6b717045a3471dffb475e8398139be49f800
Justin Silvestre <justinsilvestre@gmail.com>
2021-04-02 10:48:13 +0200

    Try setting headers with only next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,34 @@
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
-module.exports = withPWA({
+const configWithPWA = withPWA({
   pwa: {
-    dest: 'public',
+    dest: "public",
     runtimeCaching,
   },
-})
+});
+
+console.log("booop", configWithPWA.headers);
+
+const config = {
+  ...configWithPWA,
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = config;
